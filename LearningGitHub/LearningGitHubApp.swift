d24bb692f3eae83f129502ae958d536727fb32ee
salima <salimaounoughi@gmail.com>
2022-06-22 15:36:54 +0200

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Salima O. on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
